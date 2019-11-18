092cb52de3204a7c2c56872a531a656aac6c0c90
obabur <obabur@wlan-047005.nbw.tue.nl>
2019-11-18 11:21:44 +0100

    added todo to fix the absolute model folder path

--- src/main/java/com/services/ModelController.java
+++ src/main/java/com/services/ModelController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RestController 
 @RequestMapping("/rest")
 public class ModelController {
+	// TODO @Sinem make this configurable
 	  public final String FILE_PATH="C:\\Users\\sinem\\Desktop\\ATLZoo";	  
  
 		 @GetMapping("/folders")
